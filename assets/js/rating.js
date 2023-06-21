0c307e9c54b03782c0a45a24df709e635e24bbc7
Carlos Eduardo <68128063+Carlosmax1@users.noreply.github.com>
2023-06-20 22:13:11 -0300

    evento individual
    finalização da parte visual da página do evento.

--- assets/js/rating.js
+++ assets/js/rating.js
@@ -1,8 +1,54 @@
-// You can use JavaScript to handle the selected rating
-var ratingInputs = document.querySelectorAll('.stars input[type="radio"]');
+const starDiv = document.querySelectorAll(".stars");
 
-ratingInputs.forEach(function (input) {
-  input.addEventListener("change", function () {
-    var rating = this.value;
-  });
+starDiv.forEach((item) => {
+  const random = getRandomNumber();
+  generateStars(item, random);
 });
+
+function getRandomNumber() {
+  var wholeNumber = Math.floor(Math.random() * 6);
+  var decimal = Math.random() < 0.5 ? 0 : 0.5;
+  if (wholeNumber === 5) {
+    decimal = 0;
+  }
+  return wholeNumber + decimal;
+}
+
+function generateStars(starDiv, number) {
+  const result = divideFloatNumber(number);
+
+  const integerPart = result[0];
+  const decimalPart = result[1];
+
+  for (let i = 0; i < integerPart; i++) {
+    const II = document.createElement("i");
+    II.classList.add("ph-fill");
+    II.classList.add("ph-star");
+    starDiv.appendChild(II);
+  }
+
+  /* Adiciona a parte decimal das estrelas */
+
+  if (Math.round(decimalPart) > 0) {
+    const II = document.createElement("i");
+    II.classList.add("ph-fill");
+    II.classList.add("ph-star-half");
+    starDiv.appendChild(II);
+  }
+
+  /* for para adicionar as estrelas não preenchidas */
+
+  for (let i = 0; i < 5 - (integerPart + Math.round(decimalPart)); i++) {
+    const II = document.createElement("i");
+    II.classList.add("ph");
+    II.classList.add("ph-star");
+    starDiv.appendChild(II);
+  }
+}
+
+function divideFloatNumber(number) {
+  const integerPart = Math.floor(number);
+  const decimalPart = number - integerPart;
+
+  return [integerPart, decimalPart];
+}
